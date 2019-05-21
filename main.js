22ab83adf24b2bc8d348551e9987dd22b73085d3
royweb <xytank@gmail.com>
2019-05-21 11:36:57 -0400

    Refactor code

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-// the container of the object
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-// how to view the object
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    // how close to the object
-    0.1,
-    // how far from the object
-    1000
-);
+function init(){
+    // the container of the object
+    scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
+    // how to view the object
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        // how close to the object
+        0.1,
+        // how far from the object
+        1000
+    );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+    // antialias make the cube sharp
+    renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// show the object to the html
-document.body.appendChild(renderer.domElement);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-// create the box geometry
-// the size of the box geometry
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( {color: 0x9ab9d4} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    // show the object to the html
+    document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+    // create the box geometry
+    // the size of the box geometry
+    const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    const material = new THREE.MeshBasicMaterial( {color: 0x9ab9d4} );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+}
 
 // this function run like a loop
 function animate(){
@@ -33,10 +38,11 @@ function animate(){
     requestAnimationFrame(animate);
 
     // rotate the cube based on axis by speed
-    cube.rotation.x += 0.03;
-    cube.rotation.y += 0.03;
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
 
     renderer.render(scene, camera);
 }
 
+init();
 animate();
